b744e5fc5f0ddb6b419ebc801f89bd84330381a0
SWIFT 3 <albionprenga21@gamil.com>
2022-09-30 22:49:27 +0200

    api changes

--- lib/utils/weatherUtils.dart
+++ lib/utils/weatherUtils.dart
@@ -1,6 +1,6 @@
 const OPEN_WEATHER_BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
 const OPEN_WEATHER_4DAY_URL = 'https://api.openweathermap.org/data/2.5/forecast';
-const OPEN_WEATHER_API_KEY = '911ffaf80a9fa826a472acd5ceddbadc';
+const OPEN_WEATHER_API_KEY = 'api key on openwether';
 
 
 String prepareWeatherUrl(String long, String lat) {
@@ -14,7 +14,3 @@ String prepareHourlyWeather(String long, String lat) {
   return url;
 }
 
-
-// var url = '
-// https://api.openweathermap.org/data/2.5/weather?lat=41.73&lon=19.64&appid=911ffaf80a9fa826a472acd5ceddbadc';
-// https://api.openweathermap.org/data/3.0/onecall?lat=41.73&lon=19.64&exclude=hourly&appid=911ffaf80a9fa826a472acd5ceddbadc';
